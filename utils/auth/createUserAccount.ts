43cfbc6a7c44b134cb1d852ae019bc80983fdf8a
Karuppusamy <d.karuppusamy@outlook.com>
2023-06-12 04:44:16 +0530

    Add functions to create and validate firebase user

---
+++ utils/auth/createUserAccount.ts
@@ -0,0 +1,38 @@
+"use server";
+import axios, { AxiosResponse } from "axios";
+
+type ResponseData = {
+  localId: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+};
+
+// https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+// https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/signInWithPassword
+export const createUserAccount = async (
+  email: string,
+  password: string
+): Promise<string | null> => {
+  const apiKey = process.env.FIREBASE_API_KEY;
+
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=
+  const signupUrl = `${process.env.FIREBASE_IDENTITY_BASE_URL}signUp?key=${apiKey}`;
+
+  if (!signupUrl?.length || !apiKey?.length) return null;
+
+  const response: AxiosResponse<ResponseData> = await axios.post(signupUrl, {
+    email,
+    password,
+    returnSecureToken: true, // This returns the refresh token
+  });
+
+  if (parseInt(response.status.toString()) === 200) {
+    const userId = response.data.localId ?? null;
+
+    return userId;
+  }
+
+  return null;
+};
---
+++ utils/auth/validateUserAccount.ts
@@ -0,0 +1,43 @@
+"use server";
+import axios, { AxiosResponse } from "axios";
+
+type ResponseData = {
+  localId: string;
+  idToken: string;
+  email: string;
+  displayName: string;
+  refreshToken: string;
+  expiresIn: string;
+  registered: boolean;
+};
+
+// https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+// https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/signInWithPassword
+export const validateUserAccount = async (
+  email: string,
+  password: string
+): Promise<string | null> => {
+  const apiKey = process.env.FIREBASE_API_KEY;
+
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=
+  const verifyPasswordUrl = `${process.env.FIREBASE_IDENTITY_BASE_URL}signInWithPassword?key=${apiKey}`;
+
+  if (!verifyPasswordUrl?.length || !apiKey?.length) return null;
+
+  const response: AxiosResponse<ResponseData> = await axios.post(
+    verifyPasswordUrl,
+    {
+      email,
+      password,
+      returnSecureToken: true, // This returns the refresh token
+    }
+  );
+
+  if (parseInt(response.status.toString()) === 200) {
+    const userId = response.data.localId ?? null;
+
+    return userId;
+  }
+
+  return null;
+};
