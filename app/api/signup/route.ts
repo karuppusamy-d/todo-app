e74917d8c71d7865506950eb268c503bb15fde6d
Karuppusamy <d.karuppusamy@outlook.com>
2023-06-12 18:14:31 +0530

    Add api for signup

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,10 +1,10 @@
 import z, { ZodError } from "zod";
-import { validateUserAccount } from "@/utils/auth/validateUserAccount";
 import { loginSchema } from "@/utils/zod/authSchema";
 import { NextResponse } from "next/server";
 import { AxiosError } from "axios";
 import { handleAuthError } from "@/utils/auth/handleAuthError";
 import { handleZodError } from "@/utils/zod/handleZodError";
+import { createUserAccount } from "@/utils/auth/createUserAccount";
 import { ApiErrorResponse } from "@/components/types/ApiErrorResponse";
 import { sign } from "jsonwebtoken";
 import { User } from "@/components/types/user";
@@ -20,6 +20,7 @@ type AuthError = {
     message: string;
   };
 };
+
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export type Params = z.infer<typeof loginSchema>;
@@ -33,7 +34,7 @@ export async function POST(request: Request) {
     return NextResponse.json(res, { status: 400 });
   }
   try {
-    const user = await validateUserAccount(auth.email, auth.password);
+    const user = await createUserAccount(auth.email, auth.password);
     if (user) {
       const access_token = sign({ uid: user.localId }, JWT_SECRET, {
         expiresIn: "24h",
---
+++ components/types/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+  email: string;
+  access_token: string;
+};
+
+export type JWTUser = {
+  uid: string;
+};
